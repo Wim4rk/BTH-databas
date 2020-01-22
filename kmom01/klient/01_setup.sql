9588851f3638213fb1ba538c5daa7966ab3f7ba3
Wim4rk <olov.wimark@gmail.com>
2020-01-22 22:09:05 +0100

    Editing example file

--- kmom01/klient/01_setup.sql
+++ kmom01/klient/01_setup.sql
@@ -5,15 +5,17 @@
 
 -- Create the user 'user' with a backward compatible password algorithm
 -- and the password 'pass'
-CREATE USER IF NOT EXISTS 'user'@'%'
-    IDENTIFIED WITH mysql_native_password
-    BY 'pass'
-;
 
--- Alternate for MariaDB 
+-- Commands for MySQL
 -- CREATE USER IF NOT EXISTS 'user'@'%'
---     IDENTIFIED BY 'pass'
+--     IDENTIFIED WITH mysql_native_password
+--     BY 'pass'
 -- ;
+
+-- Alternate for MariaDB
+CREATE USER IF NOT EXISTS 'olov'@'localhost'
+   IDENTIFIED BY 'Wim4rk'
+;
 
 -- Create user and assign a password, compatible between MySQL
 -- and MariaDB. But it may require that MySQL 10.0 is setup to use
@@ -22,11 +24,12 @@ CREATE USER IF NOT EXISTS 'user'@'%'
 -- CREATE USER IF NOT EXISTS 'user'@'%';
 -- ALTER USER 'user'@'%' IDENTIFIED BY 'pass';
 
+-- Create the database 'dbwebb', but only if it does not exists.
+CREATE DATABASE IF NOT EXISTS dbwebb;
+
 -- Grant the user all privilegies on the database.
 GRANT ALL PRIVILEGES
     ON dbwebb.*
-    TO 'user'@'%'
+    TO 'olov'@'localhost'
 ;
 
--- Create the database 'dbwebb', but only if it does not exists.
-CREATE DATABASE IF NOT EXISTS dbwebb;
