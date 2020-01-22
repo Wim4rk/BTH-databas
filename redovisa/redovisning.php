3d6f4187b294c62baee63161c3cf98ee909bf350
Wim4rk <olov.wimark@gmail.com>
2020-01-22 22:08:42 +0100

    Adding Markdown

---
+++ redovisa/redovisning.php
@@ -0,0 +1,92 @@
+<!doctype html>
+
+<?php
+    require __DIR__ . "/vendor/autoload.php";
+?>
+
+<html lang="sv">
+<head>
+<meta charset="utf-8">
+<title>Redovisning | Databas</title>
+<link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+
+
+
+<header>
+    <nav>
+        <a href="me.html">Me</a> |
+        <a href="redovisning.php">Redovisning</a> |
+        <a href="om.html">Om</a>
+    </nav>
+</header>
+
+
+
+<article>
+    <h1>Redovisning av kursmoment</h1>
+    <header>
+        <p>Snabblänkar till respektive kursmoment:</p>
+
+        <a href="#kmom01">Kmom01</a> |
+        <a href="#kmom02">Kmom02</a> |
+        <a href="#kmom03">Kmom03</a> |
+        <a href="#kmom04">Kmom04</a> |
+        <a href="#kmom05">Kmom05</a> |
+        <a href="#kmom06">Kmom06</a> |
+        <a href="#kmom07">Slutredovisning</a>
+
+    </header>
+
+    <section>
+    <?php
+        $filename = __DIR__ . "/texter/kmom01.md";
+        $text     = file_get_contents($filename);
+        $filter   = new \Anax\TextFilter\TextFilter();
+        $parsed   = $filter->parse($text, ["shortcode", "markdown"]);
+        echo $parsed->text;
+    ?>
+    </section>
+
+    <section>
+    <a id="kmom02"><h2>Kmom02</h2></a>
+    </section>
+
+    <section>
+    <a id="kmom03"><h2>Kmom03</h2></a>
+    </section>
+
+    <section>
+    <a id="kmom04"><h2>Kmom04</h2></a>
+    </section>
+
+    <section>
+    <a id="kmom05"><h2>Kmom05</h2></a>
+    </section>
+
+    <section>
+    <a id="kmom06"><h2>Kmom06</h2></a>
+    <p>Tänk, tryck, tala.</p>
+    </section>
+
+    <section>
+    <a id="kmom07"><h2>Kmom07 - 10</h2></a>
+    <p>Tänk, tryck, tala.</p>
+    </section>
+
+</article>
+
+<footer id="site-footer">
+    <h4>Valideringsverktyg</h4>
+    <p>
+        <a href="http://validator.w3.org/check/referer" target="_blank">HTML5</a> |
+        <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">CSS</a> |
+        <a href="http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance" target="_blank">Unicorn</a>
+    </p>
+</footer>
+
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ redovisa/texter/kmom01.md
@@ -0,0 +1,26 @@
+<a id="kmom01"><h2>Kmom01</h2></a>
+
+<p>Det är alltid roligt att starta upp en ny kurs. Även om det än en gång
+    skulle börja med installationsbekymmer. Av någon anledning gav jag mig
+    sjutton på att jag skulle använda MySQL, och det ville inte min version
+    av Debian. Därefter gick jag över till MariaDB, och det gick inte heller.</p>
+<p>Efter att en mycket tålmodig Mikael Roos handlett mig en stund kom vi fram
+    till att det rörde sig om en officiell bug. Det kändes ganska så bra,
+    i vanliga fall brukar det bara vara användarfel.</p>
+<p>Bekymren fortsatte. Att uppgradera från MySQL till MariaDB verkar inte
+    vara någon bra idé. Det blir en massa rester kvar av MySQL som stör,
+    alldeles oavsett om man kör <code>purge</code> eller ej.</p>
+<h3>Introduktion, labb</h3>
+<p>Jag har ju kört en hel del SQL tidigare, så själva koden är ganska enkel
+    att hänga med i för mig. När jag väl fått igång allting så är jag mycket
+    imponerad av hur smidigt det är att köra CLI-klienten. Särskilt när man
+    anropar sql-filer, antingen direkt från terminalen, eller genom att
+    ange <em>source</em> inifrån klienten.</p>
+
+<h3>Kort och koncist</h3>
+<ul>
+    <li><strong>Vilken är din TIL för detta kmom?</strong><br \>
+        <p></p></li>
+    <li><strong>Fråga?</strong><br \>
+        <p>Svar...</p></li>
+</ul>
