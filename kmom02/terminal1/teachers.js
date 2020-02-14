26ba56682f961a49d26a241c05838bb8054b29c1
Wim4rk <olov.wimark@gmail.com>
2020-02-14 17:30:29 +0100

    Database-Node excercises

---
+++ kmom02/terminal1/teachers.js
@@ -0,0 +1,62 @@
+/**
+ * Show teachers and their departments.
+ */
+"use strict";
+
+const mysql = require("promise-mysql");
+const config = require("./config.json");
+
+/**
+ * Main function.
+ * @async
+ * @returns void
+ */
+(async function() {
+    const db = await mysql.createConnection(config);
+    let sql;
+    let res;
+    let data;
+
+    sql = `
+        SELECT
+            akronym,
+            fornamn,
+            efternamn,
+            avdelning,
+            lon
+        FROM larare
+        ORDER BY akronym;
+    `;
+    res = await db.query(sql);
+
+    data = JSON.stringify(res, null, 4);
+
+    // Output as formatted text in table
+let str;
+
+str  = "+-----------+---------------------+-----------+----------+\n";
+str += "| Akronym   | Namn                | Avdelning |   Lön    |\n";
+str += "|-----------|---------------------|-----------|----------|\n";
+
+    // Loop through each row the resultset
+for (const row of res) {
+    // console.info(row);
+
+    str += "| ";
+    str += row.akronym.padEnd(10);
+    str += "| ";
+    str += (row.fornamn + " " + row.efternamn).padEnd(20);
+    str += "| ";
+    str += row.avdelning.padEnd(10);
+    str += "| ";
+    str += row.lon.toString().padStart(8);
+    str += " |\n";
+}
+
+str  += "+-----------+---------------------+-----------+----------+\n";
+
+    // console.info(data);
+    console.info(str);
+
+    db.end();
+})();
